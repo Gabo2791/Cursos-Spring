8bd7b56f17750fe3122f9d106411cc60d4b8cd9b
Gabo2791 <68802348+Gabo2791@users.noreply.github.com>
2020-08-10 02:33:24 -0500

    Se agregó dependencias de seguridad al proyecto. uso de Spring Security

---
+++ Leccion_10/src/main/java/mx/com/gm/web/WebConfig.java
@@ -0,0 +1,39 @@
+package mx.com.gm.web;
+
+import java.util.Locale;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+//import lombok.var;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		var slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(new Locale("es"));
+		return slr;
+	}
+
+	// Configurar Interceptor para cambiar el idioma de forma dinamica
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		var lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	//Registrar Interceptor
+	@Override
+	public void addInterceptors(InterceptorRegistry registro) {
+		registro.addInterceptor(localeChangeInterceptor());
+	}
+}
